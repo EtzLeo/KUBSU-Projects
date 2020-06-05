59ded5a2eced0e94875b7c496f473653c4d23ab6
Kseniia <kbasloviak@gmail.com>
2020-06-05 10:13:18 +0300

    Add form and form elements

--- IZ-11/IZ-11/Program.fs
+++ IZ-11/IZ-11/Program.fs
@@ -4,7 +4,26 @@
 open System
 open System.Windows.Forms
 
-[<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+let form = new Form(Width=300, Height=500, Text="Калькулятор")
+
+let leftTextBox = new TextBox(Width=100, Height=50, Top=200, Left=10)
+form.Controls.Add(leftTextBox)
+
+let rightTextBox = new TextBox(Width=100, Height=50, Top=200, Left=180)
+form.Controls.Add(rightTextBox)
+
+let addButton = new Button(Width=50, Height=50, Top=200, Left=120, Text="+")
+form.Controls.Add(addButton)
+
+let subButton = new Button(Width=50, Height=50, Top=250, Left=120, Text="-")
+form.Controls.Add(subButton)
+
+let divButton = new Button(Width=50, Height=50, Top=150, Left=120, Text="/")
+form.Controls.Add(divButton)
+
+let mulButton = new Button(Width=50, Height=50, Top=100, Left=120, Text="*")
+form.Controls.Add(mulButton)
+
+
+
+do Application.Run(form)
