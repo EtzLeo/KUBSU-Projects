39a38b16e39319513fdcb2590ef4b40fdee6563c
Kseniia <kbasloviak@gmail.com>
2020-06-05 09:58:21 +0300

    Create project 11

---
+++ IZ-11/IZ-11/Program.fs
@@ -0,0 +1,10 @@
+﻿//Разработать  программу,  реализующую  простые  вычис-ление над двумя действительными числами (сложения, вычита-ния, деления, умножения). 
+//Учесть все возможные ошибки.
+
+open System
+open System.Windows.Forms
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    0 // return an integer exit code
